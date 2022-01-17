41ca595bc8bdf4b3ad9893dab76a0a1a3f49e04a
FishGoddess <1149062639@qq.com>
2022-01-18 01:45:08 +0800

    完善

--- errors.go
+++ errors.go
@@ -7,29 +7,42 @@ import (
 )
 
 const (
-	codeBadRequest          = http.StatusBadRequest          // 请求错误的错误码
-	codeNotFound            = http.StatusNotFound            // 找不到的错误码
-	codeServerInternalError = http.StatusInternalServerError // 系统内部错误的错误码
-	codeDBError             = 1100                           // 数据库错误的错误码
-	codePageTokenInvalid    = 1200                           // 非法的分页令牌的错误码
+	codeBadRequest          = http.StatusBadRequest
+	codeUnauthorized        = http.StatusUnauthorized
+	codeForbidden           = http.StatusForbidden
+	codeNotFound            = http.StatusNotFound
+	codeRequestTimeout      = http.StatusRequestTimeout
+	codeTeapot              = http.StatusTeapot // :)
+	codeTooManyRequests     = http.StatusTooManyRequests
+	codeServerInternalError = http.StatusInternalServerError
+	codeServiceUnavailable  = http.StatusServiceUnavailable
+
+	codeTokenInvalid = 1000
+	codeDBError      = 1100
 )
 
-// Error 是错误结构体
+var (
+	// FormatError formats e and returns a string.
+	FormatError = func(e *Error) string {
+		return fmt.Sprintf("%d (%s)", e.code, e.err.Error())
+	}
+)
+
+// Error wraps err with code.
 type Error struct {
 	err  error
 	code int32
 }
 
-// Error 返回错误信息
+// Error returns the msg of e.
 func (e *Error) Error() string {
 	if e == nil || e.err == nil {
 		return ""
 	}
-
-	return fmt.Sprintf("%d (%s)", e.code, e.err.Error())
+	return FormatError(e)
 }
 
-// Is 判断 e 是否是 target 类型的错误
+// Is returns if e has the same type of target.
 func (e *Error) Is(target error) bool {
 	if e == nil {
 		return e == target
@@ -43,7 +56,7 @@ func (e *Error) Is(target error) bool {
 	return e.code == err.code
 }
 
-// Unwrap 返回内部的错误
+// Unwrap returns err inside.
 func (e *Error) Unwrap() error {
 	if e == nil {
 		return nil
@@ -52,12 +65,7 @@ func (e *Error) Unwrap() error {
 	return e.err
 }
 
-// ErrorInfo 详细错误信息
-func (e *Error) ErrorInfo() (int32, error) {
-	return e.code, e.err
-}
-
-// WithCode 将 code 包装进 error
+// WithCode wraps err with code
 func WithCode(err error, code int32) error {
 	if err == nil {
 		return nil
@@ -69,7 +77,7 @@ func WithCode(err error, code int32) error {
 	}
 }
 
-// Is 判断 err 是否是 code 类型的错误
+// Is returns if err is Error and its code == code.
 func Is(err error, code int32) bool {
 	for {
 		if err == nil {
@@ -89,60 +97,102 @@ func Is(err error, code int32) bool {
 	}
 }
 
-func GetRawError(err error) *Error {
-	ee, ok := err.(*Error)
-	if ok {
-		return ee
-	}
-	return &Error{fmt.Errorf("UnKnow error : %v", err), 9999}
-}
-
-// BadRequest 返回请求错误
+// BadRequest returns a bad request error.
 func BadRequest(err error) error {
 	return WithCode(err, codeBadRequest)
 }
 
-// IsBadRequest 判断 err 是不是请求错误
+// IsBadRequest if err is bad request.
 func IsBadRequest(err error) bool {
 	return Is(err, codeBadRequest)
 }
 
-// NotFound 返回找不到的错误
+// Unauthorized returns a unauthorized error.
+func Unauthorized(err error) error {
+	return WithCode(err, codeUnauthorized)
+}
+
+// IsUnauthorized if err is unauthorized.
+func IsUnauthorized(err error) bool {
+	return Is(err, codeUnauthorized)
+}
+
+// Forbidden returns a forbidden error.
+func Forbidden(err error) error {
+	return WithCode(err, codeForbidden)
+}
+
+// IsForbidden if err is forbidden.
+func IsForbidden(err error) bool {
+	return Is(err, codeForbidden)
+}
+
+// NotFound returns a not found error.
 func NotFound(err error) error {
 	return WithCode(err, codeNotFound)
 }
 
-// IsNotFound 判断 err 是不是找不到的错误
+// IsNotFound if err is not found.
 func IsNotFound(err error) bool {
 	return Is(err, codeNotFound)
 }
 
-// ServerInternalError 返回系统内部错误
+// RequestTimeout returns a request timeout error.
+func RequestTimeout(err error) error {
+	return WithCode(err, codeRequestTimeout)
+}
+
+// IsRequestTimeout if err is request timeout.
+func IsRequestTimeout(err error) bool {
+	return Is(err, codeRequestTimeout)
+}
+
+// TooManyRequests returns a too many requests error.
+func TooManyRequests(err error) error {
+	return WithCode(err, codeTooManyRequests)
+}
+
+// IsTooManyRequests if err is too many requests.
+func IsTooManyRequests(err error) bool {
+	return Is(err, codeTooManyRequests)
+}
+
+// ServerInternalError returns a server internal error.
 func ServerInternalError(err error) error {
 	return WithCode(err, codeServerInternalError)
 }
 
-// IsServerInternalError 判断 err 是不是系统内部错误
+// IsServerInternalError if err is server internal request.
 func IsServerInternalError(err error) bool {
 	return Is(err, codeServerInternalError)
 }
 
-// DBError 返回数据库错误
+// ServiceUnavailable returns a service unavailable error.
+func ServiceUnavailable(err error) error {
+	return WithCode(err, codeServiceUnavailable)
+}
+
+// IsServiceUnavailable if err is service unavailable.
+func IsServiceUnavailable(err error) bool {
+	return Is(err, codeServiceUnavailable)
+}
+
+// TokenInvalid returns a token invalid error.
+func TokenInvalid(err error) error {
+	return WithCode(err, codeTokenInvalid)
+}
+
+// IsTokenInvalid if err is token invalid.
+func IsTokenInvalid(err error) bool {
+	return Is(err, codeTokenInvalid)
+}
+
+// DBError returns a db error.
 func DBError(err error) error {
 	return WithCode(err, codeDBError)
 }
 
-// IsDBError 判断 err 是不是数据库错误
+// IsDBError if err is db error.
 func IsDBError(err error) bool {
 	return Is(err, codeDBError)
-}
-
-// PageTokenInvalid 返回数据库错误
-func PageTokenInvalid(err error) error {
-	return WithCode(err, codePageTokenInvalid)
-}
-
-// IsPageTokenInvalid 判断 err 是不是数据库错误
-func IsPageTokenInvalid(err error) bool {
-	return Is(err, codePageTokenInvalid)
 }
